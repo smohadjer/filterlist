712f24ef4c51be478963333cc16cf2da2f49210a
Saeid Mohadjer <saeid@fastmail.fm>
2019-10-05 13:53:30 +0200

    Fixes build issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp'),
 	babel = require('gulp-babel'),
 	uglify = require('gulp-uglify'),
-	runSequence = require('run-sequence'),
 	rename = require("gulp-rename"),
 	watch = require('gulp-watch'),
 	batch = require('gulp-batch'),
@@ -20,20 +19,20 @@ gulp.task('clean:dist', function () {
 });
 
 gulp.task('lint', () => {
-    // ESLint ignores files with "node_modules" paths.
-    // So, it's best to have gulp ignore the directory as well.
-    // Also, Be sure to return the stream from the task;
-    // Otherwise, the task may end before the stream has finished.
-    return gulp.src(['src/*.js'])
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
-        .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-        .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-        .pipe(eslint.failAfterError());
+	// ESLint ignores files with "node_modules" paths.
+	// So, it's best to have gulp ignore the directory as well.
+	// Also, Be sure to return the stream from the task;
+	// Otherwise, the task may end before the stream has finished.
+	return gulp.src(['src/*.js'])
+		// eslint() attaches the lint output to the "eslint" property
+		// of the file object so it can be used by other modules.
+		.pipe(eslint())
+		// eslint.format() outputs the lint results to the console.
+		// Alternatively use eslint.formatEach() (see Docs).
+		.pipe(eslint.format())
+		// To have the process exit with an error code (1) on
+		// lint error, return the stream and pipe to failAfterError last.
+		.pipe(eslint.failAfterError());
 });
 
 gulp.task('babel', () =>
@@ -68,12 +67,12 @@ gulp.task('rollup', function() {
 		.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('compress', () => {
+gulp.task('compress', () =>
 	gulp.src('dist/*.es5.js')
 		.pipe(uglify())
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(gulp.dest('dist'))
-});
+);
 
 gulp.task('watch', () => {
 	watch('src/*.js', batch(function (events, done) {
@@ -81,19 +80,15 @@ gulp.task('watch', () => {
 	}));
 });
 
-gulp.task('serve', function(callback) {
-	runSequence(
-		['lint'],
-		['rollup'],
-		['watch'],
-		callback);
-});
+gulp.task('serve', gulp.series(
+	'lint',
+	'rollup',
+	'watch'
+));
 
-gulp.task('build', function(callback) {
-	runSequence(
-		['clean:dist'],
-		['rollup'],
-		['babel'],
-		['compress'],
-		callback);
-});
+gulp.task('build', gulp.series(
+	'clean:dist',
+	'rollup',
+	'babel',
+	'compress'
+));
